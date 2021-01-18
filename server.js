eb0e8979ea3a0da0873996fca4b7bb8f54b1ebcc
Mital-idea2code <75659841+Mital-idea2code@users.noreply.github.com>
2021-01-18 10:56:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb+srv://Mital-idea2code:Mital@idea2code@cluster0.wvtbx.mongodb.net/demoNodeApp?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,useFindAndModify: false })
 
 app.use(userRouter)
-app.listen(3000, () => { console.log('Server is running....') })
+app.listen(3000, () => { console.log('Server is running 3000....') })
